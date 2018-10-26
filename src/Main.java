8fd2e99d46598147962f3bc4d68243a9a8bd48b8
Manisha1212 <takalemanisha131@gmail.com>
2018-10-26 17:22:31 +0530

    Delete Substarction Method

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ public class Main {
     public static void main(String a[]) {
     }
 
-    void add(int number1, int nuber2) {
+    void add(int number1, int number2) {
         System.out.println("Addition of two number is = " (number1 + number2));
 
     }
 
-    static void multiplication(int number1, int nuber2) {
+    static void multiplication(int number1, int number2) {
         System.out.println("multiplication for two number:" + (number1 * number2));
     }
 
-    public static void subtractIntNumbers(int number1, int nuber2) {
-        System.out.println("Subtraction of 2 numbers is" + (number1 - number2));
-    }
 }

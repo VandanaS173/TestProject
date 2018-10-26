74d3c31030d986e7b5e05b21115513b0b50c501e
Manisha1212 <takalemanisha131@gmail.com>
2018-10-26 16:54:15 +0530

    Provide addition functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String a[]) {
+    }
+
+    void add(int a, int b) {
+        System.out.println("Addition of two number is = " + a + b;);
 
     }
 
